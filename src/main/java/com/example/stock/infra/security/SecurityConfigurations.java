6851093d51257af7c53460a2df3c13e7dfee9042
Felipe Alves <felipealexandre1408@gmail.com>
2024-01-25 16:26:58 -0300

    Corrigindo bug na permissao de user, onde usuarios comuns conseguiam fazer put e delete, quando o esperado era somente os admins conseguirem

--- src/main/java/com/example/stock/infra/security/SecurityConfigurations.java
+++ src/main/java/com/example/stock/infra/security/SecurityConfigurations.java
@@ -31,6 +31,9 @@ public class SecurityConfigurations {
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
                         .requestMatchers(HttpMethod.POST, "/products").hasRole("ADMIN")
+
+                        .requestMatchers(HttpMethod.PUT, "/products/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/products/**").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
